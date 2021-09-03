0125628b774f5a1fe076c79889fdc8baeb8b0656
EgorGryadunov <59438657+AairMaRK@users.noreply.github.com>
2021-09-03 15:52:59 +0300

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Egor Gryadunov on 16.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
